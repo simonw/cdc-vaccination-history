bc16bc96421f46cd66d95a5858a4d61c32e6341e
Alexander Ose <alexander@parsely.com>
2021-08-31 20:31:15 -0700

    Add CSV compilation script
    Try this instead
    
    Adjust iter_commits command so it works in branches

--- build_database.py
+++ build_database.py
@@ -3,9 +3,9 @@ import git
 import json
 
 
-def iterate_file_versions(repo_path, filepaths, ref="main"):
+def iterate_file_versions(repo_path, filepaths):
     repo = git.Repo(repo_path, odbt=git.GitDB)
-    commits = reversed(list(repo.iter_commits(ref, paths=filepaths)))
+    commits = reversed(list(repo.iter_commits("HEAD", paths=filepaths)))
     for commit in commits:
         blob = [b for b in commit.tree.blobs if b.name in filepaths][0]
         yield commit.committed_datetime, commit.hexsha, blob.data_stream.read()
---
+++ compile_csvs.py
@@ -0,0 +1,26 @@
+import sqlite_utils
+import csv
+
+# Outputs everything in the SQLite database into a condensed CSV files, 
+# similar in format to the [New York Times COVID-19 data](https://github.com/nytimes/covid-19-data).
+
+# Note that this must be run after build_database.py.
+
+def create_csv(name, cols):
+    cursor = db.execute("select " + ','.join(cols) + " from " + name); 
+    rows = cursor.fetchall()
+    csvWriter = csv.writer(open(name + ".csv", "w")) 
+    for row in rows:
+        csvWriter.writerow(row)
+
+if __name__ == "__main__":
+    db = sqlite_utils.Database("cdc.db")
+
+    # Create condensed CSV for daily_reports
+    cols = ["Date", "Location", "ShortName", "LongName", "Census2019", "Series_Complete_Pop_Pct"]
+    create_csv("daily_reports", cols) 
+    
+    # Create condensed CSV for daily_reports_counties
+    cols = ["Date", "FIPS", "StateName", "StateAbbr", "County", "Series_Complete_Pop_Pct"]
+    create_csv("daily_reports_counties", cols)
+
